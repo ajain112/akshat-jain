19d511a202b69ca5295b7a0ce4ef75b660132e37
ajain112 <ajain14142@gmail.com>
2025-10-28 01:21:36 -0500

    corrected the vite file to correct the deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { SitemapPlugin } from 'vite-plugin-sitemap';
+
 
 export default defineConfig({
   plugins: [
     react(),
-    SitemapPlugin({
-      baseUrl: 'https://ajain.live',
-      routes: [
-        '/', '/projects', '/about', '/contact'
-      ]
-    })
-  ]
+  ],
 });
